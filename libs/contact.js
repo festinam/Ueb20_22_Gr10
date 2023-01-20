be58d1d76822d7e433f9ea945ae173e665b7e079
festinam <festinamjeku1@gmail.com>
2023-01-20 12:06:37 +0100

    new file

---
+++ libs/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 9
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "ke nje emer apo jo?",
+                    minlength: "emri juaj duhet te kete se paku 2 shkronja"
+                },
+                subject: {
+                    required: "ke nje teme?",
+                    minlength: "tema duhet te permbaje se paku 4 shkronja"
+                },
+                number: {
+                    required: "ke nje numer apo jo?",
+                    minlength: "numri juaj duhet te permbaje se paku 9 numra"
+                },
+                email: {
+                    required: "s'ka email, s'ka mesazh"
+                },
+                message: {
+                    required: "duhet te shkruash dicka qe te mund ta dergosh kete formular",
+                    minlength: "vetem kaq?"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
